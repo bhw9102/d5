9088e4941bbc8793c6dd712e8224a83d4e4b16db
배현우 <bhw9102@gmail.com>
2023-08-27 13:35:41 +0900

    feature: account 테이블 생성

---
+++ application/django/d5/account/resources/account.sql
@@ -0,0 +1,12 @@
+create table account
+(
+    id            integer not null
+        constraint account_id
+            primary key,
+    key           integer not null
+        constraint account_key
+            unique,
+    primary_email integer,
+    password      integer,
+    display_name  integer
+);
