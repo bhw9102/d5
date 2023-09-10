4fe11cc1a8395563615b7eb374882aad5794b93e
배현우 <bhw9102@gmail.com>
2023-09-10 18:19:31 +0900

    red: 티켓키로 티켓을 가져올 수 있다

--- domain/ticket/test/service/ticket_processor_test.py
+++ domain/ticket/test/service/ticket_processor_test.py
@@ -28,6 +28,24 @@ class TicketProcessorTest(unittest.TestCase):
         self.assertEqual(result.account_key, account_key)
         self.assertEqual(result.subject, subject)
 
+    def test_티켓키로_티켓을_가져올_수_있다(self):
+        # Arrange
+        account_key = uuid.uuid4()
+        subject = "티켓 작성 테스트"
+        command = TicketUseCaseCreateCommand(
+            account_key=account_key,
+            subject=subject
+        )
+        ticket = self.ticket_processor.create(command=command)
+
+        # Action
+        result = self.ticket_processor.get_by_key(key=ticket.key)
+
+        # Assertion
+        self.assertEqual(result.key, ticket.key)
+        self.assertEqual(result.account_key, ticket.account_key)
+        self.assertEqual(result.subject, ticket.subject)
+
     def test_(self):
         # Arrange
         # Action
