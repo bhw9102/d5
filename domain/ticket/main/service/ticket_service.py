33fc1e8d68135d62e2dd1e97bc6be3496039e8ac
배현우 <bhw9102@gmail.com>
2023-09-10 16:59:11 +0900

    feature: ticketprocessor 가 ticketusecase 를 상속받습니다.

--- domain/ticket/main/service/ticket_service.py
+++ domain/ticket/main/service/ticket_service.py
@@ -2,18 +2,21 @@ import uuid
 
 from typing import List
 
-from repository.ticket_repository import TicketRepository
-from usecase.ticket_usecase import TicketUseCase
+from ..entity.ticket import Ticket
+from ..repository.ticket_repository import TicketRepository
+from ..usecase.ticket_usecase import TicketUseCase
+from ..contract.ticket_use_case_create_command import TicketUseCaseCreateCommand
 
 
-class TicketProcessor: TicketUseCase
-	def __init__(self, repository):
+class TicketProcessor(TicketUseCase):
+	def __init__(self, repository: TicketRepository):
 		self.repository = repository
+
 	def get_by_key(self, key: uuid.UUID) -> Ticket:
 		pass
-	def find_by_account_key(self, account_key: uuid.UUID) -> List[Ticket]:
+
+	def find_all_by_account_key(self, account_key: uuid.UUID) -> List[Ticket]:
 		pass
-	def create(self, command) -> Ticket:
-		pass
-	def done(self, key: uuid.UUID) -> Ticket:
+
+	def create(self, command: TicketUseCaseCreateCommand) -> Ticket:
 		pass
