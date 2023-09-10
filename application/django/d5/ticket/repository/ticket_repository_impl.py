42cab54ffe492acd2028e99d671c3b24ba492531
배현우 <bhw9102@gmail.com>
2023-09-10 20:55:02 +0900

    feature: 레포 구현체를 작성합니다.

--- application/django/d5/ticket/repository/ticket_repository_impl.py
+++ application/django/d5/ticket/repository/ticket_repository_impl.py
@@ -3,17 +3,23 @@ from typing import List
 
 from domain.ticket.main.entity.ticket import Ticket
 from domain.ticket.main.repository.ticket_repository import TicketRepository
+from domain.ticket.main.exception.does_not_exists_ticket_exception import DoesNotExistsTicketException
 
 from ..models import TicketDataModel
 
 
 class TicketRepositoryImpl(TicketRepository):
     def save(self, ticket: Ticket) -> Ticket:
-        pass
+        ticket_data_model = TicketDataModel.of(ticket=ticket)
+        ticket_data_model.save()
+        return ticket_data_model.to_entity()
 
     def find_all_by_account_key(self, account_key: uuid.UUID) -> List[Ticket]:
-        pass
+        return [item.to_entity() for item in TicketDataModel.objects.filter(account_key=account_key)]
 
     def get_by_key(self, key: uuid.UUID) -> Ticket:
-        pass
+        try:
+            return TicketDataModel.objects.get(key=key).to_entity()
+        except TicketDataModel.DoesNotExist:
+            raise DoesNotExistsTicketException()
 
