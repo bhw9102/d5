43f0d92dd885bb96659907347a79f3ecc7990e35
배현우 <bhw9102@gmail.com>
2023-09-11 21:16:49 +0900

    feature: 신규 티켓을 작성합니다.

--- application/django/d5/ticket/urls.py
+++ application/django/d5/ticket/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .controller.ticket_controller import TicketController, get_tickets
+from .controller.ticket_controller import get_tickets
+from .controller.ticket_controller import TicketCreateController
 
 
 urlpatterns = [
     path('', get_tickets, name='tickets'),
-    path('create', TicketController.create_ticket, name='create-ticket'),
+    path('create', TicketCreateController.as_view(), name='create-ticket'),
 ]
