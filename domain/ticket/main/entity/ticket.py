34b4fc0039935e1eb7502f5a82ec6a7253d6bf7a
배현우 <bhw9102@gmail.com>
2023-10-08 11:41:34 +0900

    feature: 티켓의 상태 변경에 따른 시간을 기록합니다.

--- domain/ticket/main/entity/ticket.py
+++ domain/ticket/main/entity/ticket.py
@@ -1,7 +1,9 @@
+import datetime
 import uuid
 
 from typing import Type
 from typing import TypeVar
+from typing import Optional
 
 from ..valueobject.ticket_status import TicketStatus
 
@@ -15,12 +17,18 @@ class Ticket:
             key: uuid.UUID,
             account_key: uuid.UUID,
             status: TicketStatus,
-            subject: str
+            subject: str,
+            created_at: datetime.datetime,
+            updated_at: datetime.datetime,
+            done_at: Optional[datetime.datetime, None]
     ):
         self.key = key
         self.account_key = account_key
         self.status = status
         self.subject = subject
+        self.created_at = created_at,
+        self.updated_at = updated_at,
+        self.done_at = done_at
 
     # companion object
     @classmethod
@@ -30,12 +38,19 @@ class Ticket:
             account_key: uuid.UUID,
             subject: str
     ) -> T:
+        now = datetime.datetime.now()
         return cls(
             key=key,
             account_key=account_key,
             status=TicketStatus.READY,
-            subject=subject
+            subject=subject,
+            created_at=now,
+            updated_at=now,
+            done_at=None
         )
 
     def done(self):
+        now = datetime.datetime.now()
         self.status = TicketStatus.DONE
+        self.done_at = now
+        self.updated_at = now
